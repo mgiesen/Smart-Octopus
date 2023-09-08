6c5f65f1f18cbcb8e3c4160246fdbb72e8d7fd26
Maximilian Giesen <maximilian.giesen@sls.siempelkamp.com>
2023-09-08 21:26:07 +0200

    Initial Upload

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# SmartOctopus
-Smart Octopus ist ein browserbasiertes Gedächtnisspiel, das Arbeitsgedächtnis und mentale Rechenfähigkeiten trainiert, indem es den Spieler herausfordert, eine kurz angezeigte Zahlenfolge nicht nur zu merken, sondern auch um eine Einheit zu erhöhen.
+# Smart Octopus
+
+## Über das Spiel 🎯
+
+Smart Octopus ist ein Mini-Spiel im Browser, konzipiert um kognitive Fähigkeiten wie Arbeitsgedächtnis und mentale Rechenfertigkeiten zu fördern. Je nach gewähltem Schwierigkeitsgrad wird dem Benutzer eine Sequenz von Zahlen für eine begrenzte Zeit präsentiert. Die Aufgabe besteht darin, sich nicht nur die Zahlen zu merken, sondern sie auch mental um eins zu erhöhen und dann die modifizierte Sequenz einzugeben.
+
+## Wie spielt man? 🎮
+
+1.  **Spielbeginn**: Klicken Sie auf den "Power Button", um das Spiel zu starten.
+2.  **Schwierigkeitsgrad wählen**: Durch einen Klick auf den Octopus können Sie den Schwierigkeitsgrad einstellen. Die Schwierigkeitsstufen sind wie folgt aufgebaut:
+
+    - Retarded Octopus: 3 Zahlen, Anzeigedauer 2000 ms
+    - Stupid Octopus: 4 Zahlen, Anzeigedauer 2000 ms
+    - Basic Octopus: 4 Zahlen, Anzeigedauer 1500 ms
+    - Smart Octopus: 5 Zahlen, Anzeigedauer 1000 ms
+    - Clever Octopus: 6 Zahlen, Anzeigedauer 1500 ms
+
+3.  **Beobachten Sie die Zahlen**: Eine Zahlenreihe wird für eine begrenzte Zeit angezeigt.
+4.  **Addieren Sie die Zahlen**: Nach dem Verschwinden der Zahlen erhöhen Sie jede Zahl für sich genommen um eins und geben die neue Sequenz ein.
+5.  **Eingabe**: Verwenden Sie die Zahlenfelder am unteren Bildschirmrand. Korrekte Eingaben werden grün, falsche rot markiert.
+6.  **Spielende**: Die Runde endet, sobald alle Zahlen eingegeben wurden. Danach beginnt automatisch eine neue Runde.
+
+**Hinweis**: Bei einer angezeigten 9 muss eine 0 eingegeben werden.
+
+## Motivation 💡
+
+Smart Octopus basiert auf dem Prinzip der „dualen Aufgabe“. Es fordert nicht nur das Arbeitsgedächtnis durch das Merken von Zahlen, sondern auch die mentalen Rechenfertigkeiten durch die Addition.
+
+Das Spiel beansprucht das Arbeitsgedächtnis intensiv, indem es den Spieler dazu anregt, mehrere Zahlen simultan im Kopf zu behalten und gleichzeitig mathematische Operationen auszuführen. Forschungsergebnisse in der kognitiven Psychologie deuten darauf hin, dass derartige Übungen das Arbeitsgedächtnis stärken können.
+
+Inspiration für das Spiel war ein [Veritasium Video](https://www.youtube.com/watch?v=UBVV8pch1dM) vom 02.03.2017. Die Theoretische Grundlage stammt aus dem Buch "Schnelles Denken, langsames Denken" von Daniel Kahneman.
+
+## Zum Quellcode 🛠️
+
+Ursprünglich als schnelles Abendprojekt konzipiert, war der Code nicht für eine Veröffentlichung auf GitHub vorgesehen. Eine Refaktorisierung sowie eine verbesserte Dokumentation sind notwendig. Zukünftige Erweiterungen sind denkbar. Beiträge zur Weiterentwicklung sind herzlich willkommen.
---
+++ js/game.js
@@ -0,0 +1,353 @@
+var game_mode = "compare";
+var game_difficulty = 3;
+var watching_duration = -1;
+
+var puzzle = [];
+var digit_cursor = 0;
+var input_enabled = false;
+
+set_difficulty(3);
+
+function generate_stars(count)
+{
+
+    var container = document.getElementById("difficulty_display");
+
+    container.innerHTML = "";
+
+    for (let i = 0; i < count; i++)
+    {
+        var digit = document.createElement("div");
+        container.append(digit);
+    }
+
+}
+
+function set_difficulty(difficulty)
+{
+
+    let label = document.getElementById("difficulty");
+
+    switch (difficulty)
+    {
+        case 1:
+            label.innerText = "Retarded";
+            watching_duration = 2000;
+            generate_digits(3);
+            break;
+        case 2:
+            label.innerText = "Stupid";
+            watching_duration = 2000;
+            generate_digits(4);
+            break;
+        case 3:
+            label.innerText = "Basic";
+            watching_duration = 1500;
+            generate_digits(4);
+            break;
+        case 4:
+            label.innerText = "Smart";
+            watching_duration = 1000;
+            generate_digits(5);
+            break;
+        case 5:
+            label.innerText = "Clever";
+            watching_duration = 1500;
+            generate_digits(6);
+            break;
+    }
+
+    if (difficulty < 6)
+    {
+        game_difficulty++;
+    } else
+    {
+        game_difficulty = 1;
+    }
+
+    generate_stars(difficulty);
+
+}
+
+function generate_digits(count)
+{
+
+    var container = document.getElementById("digits");
+
+    container.innerHTML = "";
+
+    for (let i = 0; i < count; i++)
+    {
+        var digit = document.createElement("h1");
+        digit.innerText = "?";
+        container.append(digit);
+    }
+
+}
+
+document.getElementById("page_title").addEventListener("click", function ()
+{
+
+    set_difficulty(game_difficulty);
+    start_new_game();
+
+});
+
+function check_digit(input)
+{
+
+    var digits = document.getElementById("digits").getElementsByTagName("h1");
+    digits[digit_cursor].innerText = input.innerText;
+
+    let correct_answer = puzzle[digit_cursor] == 9 ? 0 : puzzle[digit_cursor] + 1;
+
+    if (input.innerText == correct_answer)
+    {
+        digits[digit_cursor].className = "correct";
+    } else
+    {
+        digits[digit_cursor].className = "wrong";
+    }
+
+}
+
+var touch_click = function (e)
+{
+
+    var digits = document.getElementById("digits").getElementsByTagName("h1");
+    var input_digit = e.target;
+
+    check_digit(input_digit);
+
+    digit_cursor++;
+
+    if (digit_cursor == digits.length)
+    {
+        setTimeout(function ()
+        {
+            start_new_game();
+        }, 500);
+    }
+
+};
+
+function enable_digit_events(trigger)
+{
+
+    var inputs = document.getElementById("touch_input").getElementsByTagName("div");
+
+    for (input of inputs)
+    {
+
+        if (input.id != "")
+        {
+            continue;
+        }
+
+        if (trigger == true)
+        {
+
+            input.addEventListener('click', touch_click, true);
+            input.style.color = "white";
+
+        } else
+        {
+
+            input.removeEventListener("click", touch_click, true);
+            input.style.color = "rgb(90, 90, 90)";
+
+        }
+
+    }
+
+    input_enabled = trigger;
+
+}
+
+function start_new_game()
+{
+
+    document.getElementById("btn_start").src = "images/reload.svg";
+
+    var digits = document.getElementById("digits").getElementsByTagName("h1");
+
+    var start_button = document.getElementById("status_icon");
+    var start_button_indicator = document.getElementById("progress_indicator");
+
+    digit_cursor = 0;
+    puzzle = [];
+
+    enable_digit_events(false);
+
+    for (var i = 0; i < digits.length; i++)
+    {
+        var rnd_digit = Math.floor(Math.random() * 10);
+        puzzle.push(rnd_digit);
+        digits[i].innerText = rnd_digit;
+        digits[i].className = "";
+    }
+
+    start_button.src = "images/eye.svg";
+
+    var start = new Date();
+    var animate = setInterval(function ()
+    {
+
+        var now = new Date();
+        var past_duration = now.getTime() - start.getTime();
+
+        if (past_duration >= watching_duration)
+        {
+
+            clearInterval(animate);
+            start_button.src = "images/keyboard.svg";
+            start_button_indicator.style.width = "0px";
+
+            for (var i = 0; i < digits.length; i++)
+            {
+                digits[i].innerText = "-";
+            }
+
+            enable_digit_events(true);
+
+        } else
+        {
+            start_button_indicator.style.width = parseInt(past_duration / watching_duration * 100) + "%";
+        }
+
+    }, 1);
+
+}
+
+var digit_objects = document.getElementById("digit_container");
+var correct_digits = [];
+
+
+var input_allowed = false;
+
+function start_game()
+{
+    var button = document.getElementById("btn_main");
+    button.classList.remove("button_effects");
+    button.onclick = "";
+
+    document.getElementById("icon_mouse").style.display = "none";
+
+    start_new_set();
+}
+
+function start_new_set()
+{
+
+    input_allowed = false;
+
+    reset_digit_correction();
+
+    document.getElementById("icon_watch").style.display = "block";
+    document.getElementById("icon_keyboard").style.display = "none";
+    document.getElementById("progress_indicator").style.display = "block";
+
+    //Generiere neue Zahlen
+    for (var digit = 0; digit < digit_objects.childElementCount; digit++)
+    {
+
+        var rnd_number = Math.floor(Math.random() * 9);
+        digit_objects.children[digit].innerHTML = rnd_number;
+
+        var sum = rnd_number + 0;
+        correct_digits[digit] = (sum <= 9 ? sum : sum - 8);
+    }
+
+    //Starte Interval
+    var start = new Date();
+    var animate = setInterval(function ()
+    {
+        var now = new Date();
+        var past_duration = now.getTime() - start.getTime();
+        if (past_duration >= watching_duration)
+        {
+            clearInterval(animate);
+            start_new_input();
+        } else
+        {
+            document.getElementById("progress_indicator").style.width = parseInt(past_duration / watching_duration * 100) + "%";
+        }
+    }, 1);
+
+}
+
+function start_new_input()
+{
+
+    input_allowed = true;
+
+    document.getElementById("icon_watch").style.display = "none";
+    document.getElementById("icon_keyboard").style.display = "block";
+    document.getElementById("progress_indicator").style.display = "none";
+
+    //Setze Eingabefelder zurück
+    for (var digit = 0; digit < digit_objects.childElementCount; digit++)
+    {
+        digit_objects.children[digit].innerHTML = "_";
+    }
+
+}
+
+function reset_digit_correction()
+{
+    for (var digit = 0; digit < digit_objects.childElementCount; digit++)
+    {
+        digit_objects.children[digit].classList.remove("wrong");
+        digit_objects.children[digit].classList.remove("correct");
+    }
+}
+
+window.addEventListener("keypress", (event) =>
+{
+    if (input_allowed)
+    {
+        for (var digit = 0; digit < digit_objects.childElementCount; digit++)
+        {
+
+            if (digit_objects.children[digit].innerHTML == "_")
+            {
+                digit_objects.children[digit].innerHTML = event.key;
+                if (event.key == correct_digits[digit])
+                {
+                    digit_objects.children[digit].classList.remove("wrong");
+                    digit_objects.children[digit].classList.add("correct");
+                } else
+                {
+                    digit_objects.children[digit].classList.add("wrong");
+                    digit_objects.children[digit].classList.remove("correct");
+                }
+
+                if (digit_objects.children[digit_objects.childElementCount - 1].innerHTML != "_")
+                {
+                    setTimeout(function ()
+                    {
+                        start_new_set();
+                    }, 500);
+                }
+
+                return;
+            }
+
+        }
+
+        if (event.key == 8)
+        {
+
+            for (var digit = 0; digit < digit_objects.childElementCount; digit++)
+            {
+
+                if (digit_objects.children[digit].innerHTML == "_")
+                {
+                    digit_objects.children[digit - 1].innerHTML = "_";
+                }
+
+            }
+        }
+    }
+});
+
